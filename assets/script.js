ebb7773c0efd9dafc0b2486de1e45cd06099250c
j-spencer11 <jackspencer0806@gmail.com>
2021-09-24 22:28:34 -0500

    added some javascript and a little more html

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,9 @@
-var clickBtn = document.getElementById("click-me");
+var quizContainer = document.getElementById("")
 
-clickBtn.addEventListener("click", function(event){
-    console.log(event);
-    var newHEadline = document.createElement("hi");
-    newHEadline.textContent = "Hello";
-    document.body.appendChild(newHEadline);
-})
  // My Questions
-var question = [
+var questionIdx = 0;
+var currentQuestion;
+var allQuestions = [
     {
     questionText: "On a vertical traffic light, where is the green light?" ,
     choices: ["Top", "Middle", "Bottom", "Right"] ,
@@ -35,6 +31,32 @@ var question = [
     },
 ];
 
-var currentQuestionIndex = -1;
+var currentQuestionsIndex = 0;
 var initialRemainingSeconds = 60;
 var remainingSeconds = initialRemainingSeconds;
+var timerCount = document.getElementById("showtimer");
+var timerClick = document.querySelector("#start-button");
+
+//variables
+var a1 = document.createElement("button");
+var a2 = document.createElement("button");
+var a3 = document.createElement("button");
+var a4 = document.createElement("button");
+var a5 = document.createElement("button");
+a1.id = "button-choice-0";
+a2.id = "button-choice-1";
+a3.id = "button-choice-2";
+a4.id = "button-choice-3";
+
+
+function displayQuestion
+
+var clickBtn = document.getElementById("click-me");
+
+clickBtn.addEventListener("click", function(event){
+    console.log(event);
+    var newHEadline = document.createElement("hi");
+    newHEadline.textContent = "Hello";
+    document.body.appendChild(newHEadline);
+})
+
